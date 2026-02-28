88ce7fe3565fdfb69b3548d83b16340b7d1ed2a2
FalkonGangrel <falkon.gangrel@gmail.com>
2026-02-28 12:16:47 -0300

    feat(Tarefa):Adicionar ResponseDTO

--- src/main/java/com/anderson/todouser/dto/TarefaResponseDTO.java
+++ src/main/java/com/anderson/todouser/dto/TarefaResponseDTO.java
@@ -1,5 +1,21 @@
 package com.anderson.todouser.dto;
 
 public class TarefaResponseDTO {
-    
+
+    private Long id;
+    private String descricao;
+    private boolean concluida;
+    private Long usuarioId;
+
+    public TarefaResponseDTO(Long id, String descricao, boolean concluida, Long usuarioId) {
+        this.id = id;
+        this.descricao = descricao;
+        this.concluida = concluida;
+        this.usuarioId = usuarioId;
+    }
+
+    public Long getId() { return id; }
+    public String getDescricao() { return descricao; }
+    public boolean isConcluida() { return concluida; }
+    public Long getUsuarioId() { return usuarioId; }
 }
